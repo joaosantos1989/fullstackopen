0cb127a4b26614b998f09163b3e77cf84f2a6b9b
João Santos <joaosantos0989@hotmail.com>
2026-08-25 17:44:56 +0100

    ex 1.13 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,70 +1,108 @@
-const Header = (props) => {
-   console.log(props)
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+//unicafe exercice
+const StatisticLine = (props) => {
+  return (
+    <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+
+  if (total === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
   return (
     <>
-    <h1>{props.course}</h1>
+      <h1>statistics</h1>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={(props.good - props.bad) / total} />
+      <StatisticLine text="positive" value={(props.good / total) * 100} />
     </>
   )
 }
 
-const Part = (props) => {
+//anecdote exercice
+const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+const Anecdote = (props) => {
   return (
-    <>
-    <p> {props.part} {props.exercises} </p>
-    </>
-  )
+      <>
+        <h1>anecdote of the day</h1>
+        <p>{props.text}</p>
+        <p>has {props.votes} votes</p>
+      </>
+    )
+  }
+
+const Vote = (props) => {
+  const copy = [... props.vote]
+
+  copy[props.selected] +=1
 }
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  )
-}
 
 const App = () => {
-  const course = {
-  name: 'Half Stack application development',
-  
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+  // unicafe exercice
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
+  //anecdotes exercice
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+  //handler votes
+  const Vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
-    <div>
-      <Header course={course.name} />
-      <Content 
-        part1={course.parts[0].name} exercises1={course.parts[0].exercises}
-        part2={course.parts[1].name} exercises2={course.parts[1].exercises}
-        part3={course.parts[2].name} exercises3={course.parts[2].exercises}
-      />
-      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
-    </div>
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+
+      <div>
+        <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+        <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text='next anecdote' />
+        <Button handleClick={Vote} text='vote' />
+      </div>
+    </>
   )
 }
 
